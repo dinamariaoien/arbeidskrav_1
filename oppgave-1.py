2f9d7a7f8ab11aec6d124bedf200acbb5a8f514d
dinamariaoien <dinamariaoien@gmail.com>
2026-09-12 20:30:52 +0200

    førsteutkast på oppgave 1, bruk av pensum og det vi har hatt i forelesning

--- oppgave-1.py
+++ oppgave-1.py
@@ -17,4 +17,74 @@ print (f"Samlet tidsbruk: {hours} timer og {remaining_minutes} minutter.")
 
 
 # Oppgave 1.2 - Analyser tekst
-text = input()
+text = input("Write a text here:")
+
+while text =="" or text == " ":
+    print("Må være tekst")
+    text = input("Write a text here:")
+
+print(len(text))
+
+text_without_spaces = text.replace(" ", "")   # kom opp automatisk
+print(len(text_without_spaces))
+
+print(text.lower())
+
+print(text[::-1])
+
+if "python" in text.lower():
+    print("Python in text")
+else:
+    print("Python not in text")
+
+
+# Oppgave 1.3
+start_value = int(input("Startverdi: "))
+end_value = int(input("Endverdi: "))
+
+while start_value > end_value:
+    print("Error")
+    start_value = int(input("Startverdi: "))
+    end_value = int(input("Endverdi: "))
+
+for number in range(start_value, end_value + 1):
+    if number % 2 == 0:
+        print(number)
+
+for number in range(start_value, end_value + 1):
+    if number % 3 == 0:
+        print(number)
+
+total = 0
+for number in range(start_value, end_value + 1):
+    total = number + total
+
+print(total)
+
+
+# Oppgave 1.4
+running = True
+
+while running:
+    print("\n1. Beregn tidspunkt")
+    print("2. Analysser tekst")
+    print("3. Analyser tallintervall")
+    print("4. Avslutt")
+
+    choice = input("Velg et alternativ (1-4): ")
+
+    if choice == "1":
+        print(f"")
+
+    elif choice == "2":
+        print(f"")
+
+    elif choice == "3":
+         print(f"")
+
+    elif choice == "4":
+        print("")
+        running = False
+
+    else:
+        print("Invalid option, please try again.")
